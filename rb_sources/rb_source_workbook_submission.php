3845a22c20baa92dbb9f3f905fa092044cf83e60
Eugene Venter <eugene@catalyst.net.nz>
2017-10-16 13:45:34 +1300

    rb fixes for totara10

--- rb_sources/rb_source_workbook_submission.php
+++ rb_sources/rb_source_workbook_submission.php
@@ -124,8 +124,7 @@ class rb_source_workbook_submission extends rb_base_source {
         $this->add_course_category_table_to_joinlist($joinlist,
             'course', 'category');
         $this->add_job_assignment_tables_to_joinlist($joinlist, 'base', 'userid');
-        $this->add_tag_tables_to_joinlist('course', $joinlist, 'course', 'id');
-        $this->add_cohort_user_tables_to_joinlist($joinlist, 'base', 'userid');
+        $this->add_core_tag_tables_to_joinlist('core', 'course', $joinlist, 'course', 'id');
         $this->add_cohort_course_tables_to_joinlist($joinlist, 'course', 'id');
 
         return $joinlist;
@@ -330,8 +329,7 @@ class rb_source_workbook_submission extends rb_base_source {
         $this->add_course_fields_to_columns($columnoptions);
         $this->add_course_category_fields_to_columns($columnoptions);
         $this->add_job_assignment_fields_to_columns($columnoptions);
-        $this->add_tag_fields_to_columns('course', $columnoptions);
-        $this->add_cohort_user_fields_to_columns($columnoptions);
+        $this->add_core_tag_fields_to_columns('core', 'course', $columnoptions);
         $this->add_cohort_course_fields_to_columns($columnoptions);
 
         return $columnoptions;
@@ -413,8 +411,7 @@ class rb_source_workbook_submission extends rb_base_source {
         $this->add_course_fields_to_filters($filteroptions);
         $this->add_course_category_fields_to_filters($filteroptions);
         $this->add_job_assignment_fields_to_filters($filteroptions);
-        $this->add_tag_fields_to_filters('course', $filteroptions);
-        $this->add_cohort_user_fields_to_filters($filteroptions);
+        $this->add_core_tag_fields_to_filters('core', 'course', $filteroptions);
         $this->add_cohort_course_fields_to_filters($filteroptions);
 
         return $filteroptions;
